1f561ba85b6bf6869aa4fc2f1b907f78b7b3bb07
Rishi Sarkar <34758607+rishi-sarkar@users.noreply.github.com>
2020-09-25 13:43:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
+input.touchA6.onEvent(ButtonEvent.Click, function () {
+    makerController.player1.press(ArcadeButton.A)
+    light.setAll(0xff0000)
+    pause(200)
+    light.setAll(0x999999)
+})
 input.touchA1.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.B)
     light.setAll(0x0000ff)
     pause(200)
     light.setAll(0x999999)
 })
-input.touchA5.onEvent(ButtonEvent.Click, function () {
-    makerController.player1.press(ArcadeButton.A)
-    light.setAll(0xff0000)
-    pause(200)
-    light.setAll(0x999999)
-})
+input.touchA1.setThreshold(685)
+input.touchA6.setThreshold(685)
+light.setAll(0x999999)
+light.setBrightness(12)
 forever(function () {
-    input.touchA1.setThreshold(850)
-    input.touchA5.setThreshold(850)
-    light.setBrightness(12)
+	
 })
