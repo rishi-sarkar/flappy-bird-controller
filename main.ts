d2d6592243d554b5f3ed12b84e835ee49e85a8fe
Rishi Sarkar <34758607+rishi-sarkar@users.noreply.github.com>
2020-09-25 11:56:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,17 @@
 input.touchA1.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.B)
-    light.showRing(
-    `white white white white white blue blue blue blue blue`
-    )
+    light.setAll(0x0000ff)
+    pause(200)
     light.setAll(0x999999)
 })
 input.touchA5.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
-    light.showRing(
-    `red red red red red white white white white white`
-    )
+    light.setAll(0xff0000)
+    pause(200)
     light.setAll(0x999999)
 })
 forever(function () {
     input.touchA1.setThreshold(850)
     input.touchA5.setThreshold(850)
-    light.setAll(0x999999)
     light.setBrightness(12)
 })
